6085495bd1951bb3579bd04ee27652302ef32581
sayedibrahimi786 <ahadibrahimi095@gmail.com>
2025-01-12 11:37:08 -0500

    refactor: remove unused Redux dependencies and Provider wrapper
    - Remove import and usage of react-redux Provider
    - Remove import of Redux store
    - Simplify component tree by removing unnecessary Redux integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 );
